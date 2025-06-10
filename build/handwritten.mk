3b7466bff4caf47f797f06506d91f81ea8ba6fbb
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-10 22:56:09 +0900

    move emojis

---
+++ build/handwritten.mk
@@ -0,0 +1,52 @@
+.PHONY: handwritten handwritten.assets handwritten.clean handwritten.check
+
+handwritten: handwritten.zip
+
+.handwritten.pre:
+	mkdir -p handwritten
+	touch .handwritten.pre
+
+handwritten.clean:
+	-rm handwritten.zip .handwritten.pre
+	-rm -rf handwritten/ && rmdir handwritten/
+
+handwritten.check: handwritten
+	.script/check_metadata_integrity.sh "handwritten"
+
+handwritten.assets= \
+  handwritten/stellar_ch_handwritten_erailtu.png \
+  handwritten/stellar_ch_handwritten_gupekorin.png \
+  handwritten/stellar_ch_handwritten_invitingbed.png \
+  handwritten/stellar_ch_handwritten_jissekikaijo.png \
+  handwritten/stellar_ch_handwritten_kawaii.png \
+  handwritten/stellar_ch_handwritten_kiskapula.png \
+  handwritten/stellar_ch_handwritten_kiskuluva.png \
+  handwritten/stellar_ch_handwritten_netemorote.png \
+  handwritten/stellar_ch_handwritten_ohayo.png \
+  handwritten/stellar_ch_handwritten_oyasumi.png \
+  handwritten/stellar_ch_handwritten_shigotogahayai.png \
+  handwritten/stellar_ch_handwritten_shigotohayasugimondai.png \
+  handwritten/stellar_ch_handwritten_square_asa.png \
+  handwritten/stellar_ch_handwritten_square_gupeko.png \
+  handwritten/stellar_ch_handwritten_square_karehanomimono.png \
+  handwritten/stellar_ch_handwritten_square_kojikiniattawa.png \
+  handwritten/stellar_ch_handwritten_square_kawaiigasugiru.png \
+  handwritten/stellar_ch_handwritten_square_kokotesutonidemasu.png \
+  handwritten/stellar_ch_handwritten_square_koresuki.png \
+  handwritten/stellar_ch_handwritten_square_nyan.png \
+  handwritten/stellar_ch_handwritten_square_okite.png \
+  handwritten/stellar_ch_handwritten_startdispute.png \
+  handwritten/stellar_ch_handwritten_sugoi.png \
+  handwritten/stellar_ch_handwritten_taitaikin.png
+
+handwritten.assets: $(handwritten.assets)
+
+handwritten.zip: $(handwritten.assets) handwritten/meta.json
+	cd handwritten/ && zip ../handwritten.zip meta.json ./*.png
+
+handwritten/meta.json: ../handwritten/meta.json .handwritten.pre
+	.script/build_metadata.sh "$<" > "$@"
+
+handwritten/%.png: ../handwritten/%.svg .handwritten.pre
+	resvg -z 4.0 --dpi 384 "$<" "$@"
+	optipng -q --fix "$@"
