92c899f398456df9ba0882182620476fe78267bd
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-11 00:50:45 +0900

    fix metadata checker

--- build/.script/check_metadata_integrity.sh
+++ build/.script/check_metadata_integrity.sh
@@ -6,11 +6,11 @@
 	printf "Checking metadata integrity...\n"
 	printf "Installed %s emoji(s).\n" "$(jq '.emojis|length' meta.json)"
 	declare -i health=0
-	jq -e '.emojis|map(.filename)|length == (unique|length)' meta.json >/dev/null || { printf "Duplicate detected.\n"; health+=1; }
+	jq -e '.emojis|map(.fileName)|length == (unique|length)' meta.json >/dev/null || { printf "Duplicate detected.\n"; health+=1; }
 	while read -r i
 	do
 		[ -e "$i" ] || { printf "File not found: %s\n" "$i"; health+=1; }
-	done  < <(jq -r '.emojis[]|.filename' meta.json)
+	done  < <(jq -r '.emojis[]|.fileName' meta.json)
 	printf "Done. %s error(s) found.\n" "${health}"
 	[ $health -eq 0 ]
 )
