f54d8af0dfa1aa87f3e2e9d42548544e93e166ff
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-11 00:40:51 +0900

    fix metadata builder
    名前が違うせいで絵文字に登録できない問題

--- build/.script/build_metadata.sh
+++ build/.script/build_metadata.sh
@@ -4,4 +4,4 @@
 	printf "build_metadata: Supports just one argument" >&2
 	exit 2
 }
-jq -cf <(cat <<<'def add_extension: def inference_extension: if .=="image/png" then ".png" elif .=="image/apng" then ".png" elif .=="image/gif" then ".gif" elif .=="image/webp" then ".webp" else ("Can'\''t recognize type \"\(.)\"."|halt_error(1)) end; .name + (.type|inference_extension); {metaVersion:2,emojis:map({filename:add_extension,downloaded:true,emoji:({host:null}+.)})}') "$1"
+jq -cf <(cat <<<'def add_extension: def inference_extension: if .=="image/png" then ".png" elif .=="image/apng" then ".png" elif .=="image/gif" then ".gif" elif .=="image/webp" then ".webp" else ("Can'\''t recognize type \"\(.)\"."|halt_error(1)) end; .name + (.type|inference_extension); {metaVersion:2,emojis:map({fileName:add_extension,downloaded:true,emoji:({host:null}+.)})}') "$1"
